493ecdefc59a57e905a849c38b585f0f63b7b042
James Stephenson <90290035+jamesastephenson@users.noreply.github.com>
2022-04-09 18:15:34 -0400

    Add files via upload

--- Pygame Projects/Pong Clone/pong.py
+++ Pygame Projects/Pong Clone/pong.py
@@ -19,6 +19,30 @@ screenHeight = 800
 screen = pygame.display.set_mode((screenWidth, screenHeight))
 pygame.display.set_caption('Pong')
 
+# Drawing
+# Display Surface - basic element, main screen we draw on (can only be a single one)
+    # can draw directly on the surface with pygame.draw
+# (Regular) Surface - Extra surface that holds material, can have as many as you want
+    # regular surfaces must be attached to the display surface
+# Rect - rectangle, you can put it around shapes and regular surfaces
+    # makes it easier to measure and manipulate regular surfaces
+    # origin of rectangle is top left, so you have to increase to move right and down
+
+# Game Rectangles
+    # pygame.Rect(startPosX, startPosY, rectWidth, rectHeight)
+# startPos's are finding the screen midpoint and subtracting half our rect width
+    # this is because of where the rectangle origin starts
+ball = pygame.Rect(screenWidth/2 - 15, screenHeight/2 - 15, 30, 30) 
+player = pygame.Rect(screenWidth - 20, screenHeight/2 - 70, 10, 140) # note, start height is half of our rect height
+opponent = pygame.Rect(10, screenHeight/2 - 70, 10, 140)
+
+# Drawing
+# pygame.draw(surface, color, rect)
+    # here surface will be our screen var, rect will be the vars we devined above
+# pygame.draw() can use rgb colors or a string of a color name
+bgColor = pygame.Color('grey12')
+lightGrey = (200, 200, 200)
+
 # Loop Section
 while True:
     # handle input
@@ -30,6 +54,15 @@ while True:
             # these two combined reliably close the game
             pygame.quit()  # uninitializes Python module
             sys.exit()  # closes the window
+
+    # Visuals
+        # note: these render in order, if you put screen.fill() at the bottom it's all you'd see
+    screen.fill(bgColor)  # filling the display surface
+    pygame.draw.rect(screen, lightGrey, player)
+    pygame.draw.rect(screen, lightGrey, opponent)
+    pygame.draw.ellipse(screen, lightGrey, ball)
+    #aaline - anti alias line, takes display surface, color, start point, endpoint
+    pygame.draw.aaline(screen, lightGrey, (screenWidth/2, 0), (screenWidth/2, screenHeight))
 
     # update the window
     pygame.display.flip()  # flip takes everything that came before it in the loop and draws it
